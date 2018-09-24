ba45a63788cd28e57bb94ccb2aedd43d9e7b0de1
Leslie Osorio <lesosorio17@gmail.com>
2018-09-24 21:45:41 +0000

    automatically backed up by learn

--- spec/catch_phrase_spec.rb
+++ spec/catch_phrase_spec.rb
@@ -1,5 +1,9 @@
 require "spec_helper"
 
-def "#catch_phrase" (phrase)
-  puts "It's-a me, Mario!"
+drequire "spec_helper"
+describe "#catch_phrase" do 
+  it "puts out a catch phrase" do 
+    phrase = "It's-a me, Mario!"
+    expect{catch_phrase(phrase)}.to output("It's-a me, Mario!\n").to_stdout
+  end
 end
